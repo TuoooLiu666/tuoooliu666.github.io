d0ffb071cb99b1a152de5d69944e4d3d8224b780
TuoooLiu666 <65417695+TuoooLiu666@users.noreply.github.com>
2025-03-21 19:47:50 +0000

    Deploying to gh-pages from @ TuoooLiu666/TuoooLiu666.github.io@ed1d6e828380a1d9876cd285317ce28e5672915f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -490,14 +490,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%74%75%6F%6F%6F%6C%69%75@%61%72%69%7A%6F%6E%61.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://orcid.org/0009-0004-6296-577X", "_blank");
         },
       },{
         id: 'social-rss',
@@ -505,13 +505,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
